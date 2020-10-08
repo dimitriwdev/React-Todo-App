d39c73badfd21663cef1527dea715d11e4fe4287
dimitri devoille <dimitri.wdev@gmail.com>
2020-10-08 14:53:21 +0100

    Add localStorage and eslintrc and prettierrc files

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,18 +2,19 @@ import React from "react";
 
 import PropTypes from "prop-types";
 
-
-const TodoItem = ({ id, labelName, completed, handleChange}) => {
+const TodoItem = ({ id, labelName, completed, handleChange, handleDelete }) => {
   return (
     <li className={completed ? "completed" : ""}>
       <div className="view">
-        <input type="checkbox" 
-        id={id} 
-        className="toggle" 
-        checked={completed}
-        onChange={() => handleChange(id)}
+        <input
+          type="checkbox"
+          id={id}
+          className="toggle"
+          checked={completed}
+          onChange={() => handleChange(id)}
         />
         <label htmlFor={id}>{labelName}</label>
+        <button className="destroy" onClick={() => handleDelete(id)} />
       </div>
     </li>
   );
@@ -24,6 +25,7 @@ TodoItem.propTypes = {
   labelName: PropTypes.string.isRequired,
   completed: PropTypes.bool.isRequired,
   handleChange: PropTypes.func.isRequired,
+  handleDelete: PropTypes.func.isRequired,
 };
 
 export default TodoItem;
